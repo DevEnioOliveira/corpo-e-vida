490768445eb767e183e0d08a41cf24c7cf44fbb7
DevEnioOliveira <eniobackup.ti@hotmail.com>
2021-04-28 02:01:11 -0300

    Completada a seção de horários

--- js/functions.js
+++ js/functions.js
@@ -23,9 +23,9 @@ $(document).ready(function(){
  
  function showSlides(n) {
    var i;
-   var slides = document.getElementsByClassName("mySlides");
-   var dots = document.getElementsByClassName("demo");
-   var captionText = document.getElementById("caption");
+   var slides = document.getelementsByClassName("mySlides");
+   var dots = document.getelementsByClassName("demo");
+   var captionText = document.getelementById("caption");
    if (n > slides.length) {slideIndex = 1}
    if (n < 1) {slideIndex = slides.length}
    for (i = 0; i < slides.length; i++) {
@@ -89,59 +89,122 @@ $(document).ready(function(){
   });
 
 
-  // SEÇÃO HORARIOS 
+  // SeÇÃO HORARIOS 
 
 
-  //fitdance
-function fitdanceIn(e) {
-  e.style.backgroundColor = '	#DEB887'
-  
-  let fitdance = document.querySelector('#fitdance');
-  fitdance.style.height = '10px'
-  fitdance.innerHTML= 'Prof Paty Rodrígues';
-  fitdance.style.textShadow = '1px 1px 1px black'
-  
+  //fitdance 30
+
+ function fitdance30() {
+  let elements = document.getElementsByClassName('fitdance30')
+  for(let i = 0; i < elements.length; i++){
+    elements[i].addEventListener('mouseover', function() {
+      this.style.backgroundColor = '#c86784'
+      this.innerHTML = 'Prof Paty Rodrigues'
+    })
+ 
+    elements[i].addEventListener('mouseout', function() {
+      this.style.backgroundColor = 'white'
+      this.innerHTML = 'FIT DANCE 30'
+    })
+  }
+ }
+
+ //fitdance
+
+ function fitdance() {
+  let elements = document.getElementsByClassName('fitdance')
+  for(let i = 0; i < elements.length; i++){
+    elements[i].addEventListener('mouseover', function() {
+      this.style.backgroundColor = '#B03B0E'
+      this.innerHTML = 'Prof Paty Rodrigues'
+    })
+ 
+    elements[i].addEventListener('mouseout', function() {
+      this.style.backgroundColor = 'white'
+      this.innerHTML = 'FIT DANCE'
+    })
+  }
+ }
+
+ //JUMP
+ function jump() {
+  let elements = document.getElementsByClassName('jump')
+  for(let i = 0; i < elements.length; i++){
+    elements[i].addEventListener('mouseover', function() {
+      this.style.backgroundColor = '#97977F'
+      this.innerHTML = 'Prof Paty Rodrigues'
+    })
+ 
+    elements[i].addEventListener('mouseout', function() {
+      this.style.backgroundColor = 'white'
+      this.innerHTML = 'JUMP'
+    })
+  }
+ }
+
+
+ //funcional
+function funcional() {
+  let elements = document.getElementsByClassName('funcional')
+  for(let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('mouseover', function() {
+      this.style.backgroundColor = '#57FAA4'
+      this.innerHTML = 'Prof Fábio'
+    })
+
+    elements[i].addEventListener('mouseout', function() {
+      this.style.backgroundColor = 'white'
+      this.innerHTML = 'FUNCIONAL'
+    })
+  }
 }
 
-function fitdanceOut(e) {
-  e.style.backgroundColor = 'white'
-  let fitdance = document.querySelector('#fitdance');
-  fitdance.innerHTML= 'FITDANCE'
+//funcional kids
+
+function funcionalKids() {
+  let elements = document.getElementsByClassName('funcionalKids')
+  for(let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('mouseover', function() {
+      this.style.backgroundColor = '#FAF557'
+      this.innerHTML = 'Prof Fábio'
+    })
+
+    elements[i].addEventListener('mouseout', function() {
+      this.style.backgroundColor = 'white'
+      this.innerHTML = 'FUNCIONAL KIDS'
+    })
+  }
 }
 
+//RPM
 
+function rpm() {
+  let elements = document.getElementsByClassName('rpm')
+  for(let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('mouseover', function() {
+      this.style.backgroundColor = '#4FADFC'
+      this.innerHTML = 'Prof Paloma'
+    })
 
-function fitdance2In(e) {
-  e.style.backgroundColor = '	#DEB887'
-  
-  let fitdance = document.querySelector('#fitdance2');
-  fitdance.style.height = '10px'
-  fitdance.innerHTML= 'Prof Paty Rodrígues';
-  fitdance.style.textShadow = '1px 1px 1px black'
-  
+    elements[i].addEventListener('mouseout', function() {
+      this.style.backgroundColor = 'white'
+      this.innerHTML = 'RPM'
+    })
+  }
 }
 
-function fitdance2Out(e) {
-  e.style.backgroundColor = 'white'
-  let fitdance = document.querySelector('#fitdance2');
-  fitdance.innerHTML= 'FITDANCE'
-}
+//GAP
+function gap() {
+  let elements = document.getElementsByClassName('gap')
+  for(let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('mouseover', function() {
+      this.style.backgroundColor = '#857F97'
+      this.innerHTML = 'Prof Paloma'
+    })
 
-
-//funcional
-
-function funcionalIn(e) {
-  e.style.backgroundColor = '	#aE2887'
-  
-  let fitdance = document.querySelector('#funcional');
-  fitdance.style.height = '10px'
-  fitdance.innerHTML= 'Prof Fábio';
-  fitdance.style.textShadow = '1px 1px 1px black'
-  
-}
-
-function funcionalOut(e) {
-  e.style.backgroundColor = 'white'
-  let fitdance = document.querySelector('#funcional');
-  fitdance.innerHTML= 'FITDANCE'
+    elements[i].addEventListener('mouseout', function() {
+      this.style.backgroundColor = 'white'
+      this.innerHTML = 'GAP'
+    })
+  }
 }
